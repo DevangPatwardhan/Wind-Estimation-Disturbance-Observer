b92d4ab999fd8d78183b708d8b8e5112abdb464e
Devang Patwardhan <patwardhandevang@gmail.com>
2026-02-14 19:06:56 +0530

    Dynamic model and synthetic winds working perfectly, time to tune LPF now

--- gust_generator.m
+++ gust_generator.m
@@ -1,15 +1,26 @@
 function W_I = gust_generator(t)
 
-% Returns wind vector in INERTIAL frame
-% Modify "mode" to switch gust types
+% Inputs:
+%   t     - time (seconds)
+%   mode  - gust type:
+%           1 = step
+%           2 = ramp
+%           3 = 1-cosine
+%           4 = ramp (fixed direction x-z plane)
+%           5 = overlay (ramp + cosine)
+%
+% Output:
+%   W_I   - 3x1 wind vector in INERTIAL frame
 
-mode = 5;   % 1=step, 2=ramp, 3=1cos, 4=random ramp, 5=overlay, 6=noisy overlay
+if nargin < 2
+    mode = 5;   % default mode
+end
 
 W_I = [0;0;0];
 
 switch mode
 
-    %% 1. Step gust (axial, z-direction)
+    %% 1. Step gust (axial z-direction)
     case 1
         t0 = 2;
         W0 = [0;0;5];
@@ -17,71 +28,77 @@ switch mode
             W_I = W0;
         end
 
-    %% 2. Ramp gust (axial, z-direction)
+    %% 2. Ramp gust (axial z-direction)
     case 2
-        t0 = 2; Tr = 3;
+        t0 = 2; 
+        Tr = 3;
         Wmax = [0;0;4];
+
         if t >= t0 && t <= t0+Tr
             W_I = Wmax*(t-t0)/Tr;
         elseif t > t0+Tr
             W_I = Wmax;
         end
 
-    %% 3. 1-Cosine gust (axial)
+    %% 3. 1-Cosine gust (axial z-direction)
     case 3
-        t0 = 3; Tg = 3;
+        t0 = 3; 
+        Tg = 3;
         Wmax = [0;0;3];
+
         if t >= t0 && t <= t0+Tg
-            W_I = 0.5*Wmax*(1-cos(pi*(t-t0)/Tg));
+            W_I = 0.5*Wmax*(1 - cos(pi*(t-t0)/Tg));
         elseif t > t0+Tg
             W_I = Wmax;
         end
 
-    %% 4. Ramp with randomized direction in x-z plane
+    %% 4. Ramp with fixed direction in x-z plane
     case 4
-        t0 = 2; Tr = 3;
-        theta = pi/6; % fixed direction (avoid time-varying randomness)
-        dir = [sin(theta);0;cos(theta)];
+        t0 = 2; 
+        Tr = 3;
+        theta = pi/6;      % fixed direction
+        dir = [sin(theta); 0; cos(theta)];
         Wmax = 4*dir;
+
         if t >= t0 && t <= t0+Tr
             W_I = Wmax*(t-t0)/Tr;
         elseif t > t0+Tr
             W_I = Wmax;
         end
 
-    %% 5. Overlay ramp + cosine (random directions)
+    %% 5. Overlay: ramp + cosine (different directions)
     case 5
+        % Directions
         theta1 = pi/4;
         theta2 = -pi/6;
 
-        dir1 = [sin(theta1);0;cos(theta1)];
-        dir2 = [sin(theta2);0;cos(theta2)];
+        dir1 = [sin(theta1); 0; cos(theta1)];
+        dir2 = [sin(theta2); 0; cos(theta2)];
 
-        % ramp
-        t0=2; Tr=3;
-        W1=[0;0;0];
-        if t>=t0 && t<=t0+Tr
-            W1=3*dir1*(t-t0)/Tr;
-        elseif t>t0+Tr
-            W1=3*dir1;
+        % --- Ramp component ---
+        t0 = 2; 
+        Tr = 3;
+        W1 = [0;0;0];
+
+        if t >= t0 && t <= t0+Tr
+            W1 = 3*dir1*(t-t0)/Tr;
+        elseif t > t0+Tr
+            W1 = 3*dir1;
         end
 
-        % cosine
-        t1=6; Tg=2;
-        W2=[0;0;0];
-        if t>=t1 && t<=t1+Tg
-            W2=2*dir2*0.5*(1-cos(pi*(t-t1)/Tg));
-        elseif t>t1+Tg
-            W2=2*dir2;
+        % --- Cosine component ---
+        t1 = 6; 
+        Tg = 2;
+        W2 = [0;0;0];
+
+        if t >= t1 && t <= t1+Tg
+            W2 = 2*dir2*0.5*(1 - cos(pi*(t-t1)/Tg));
+        elseif t > t1+Tg
+            W2 = 2*dir2;
         end
 
-        W_I=W1+W2;
+        W_I = W1 + W2;
 
-    %% 6. Noisy overlay
-    case 6
-        W_I = gust_generator_overlay(t);
-        noise_std = 0.1;
-        W_I = W_I + noise_std*randn(3,1);
 
 end
 
